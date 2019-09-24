8dd1630fe2bf5bc60393256ddaf597a01d6fe6e6
Varun Gupte <varun.gpt1007@gmail.com>
2019-09-24 18:48:57 +0530

    Update problem1.cpp

--- daily-coding-problem/problem1.cpp
+++ daily-coding-problem/problem1.cpp
@@ -1,2 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	unordered_map<int,int> m;
+	for(int i=0;i<n;i++)
+	{
+		int value;
+		cin>>value;
+		m[value]++;
+	}
+	int k;
+	cin>>k;
+	for(auto i:m)
+	{
+		int kk=k-i.first;
+		if(m.find(kk)!=m.end())
+		{
+			cout<<"true";
+			return 0;
+		}
+	}
+	cout<<"false";
+	return 0;
+}
+
+O(n)=O(1) i.e constant
